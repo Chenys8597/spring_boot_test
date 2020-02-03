74606f633a5070f7db533b5ad2140517fc23b998
JonhChen <1016380546@qq.com>
2020-02-03 11:02:58 +0800

    跨域

--- src/main/java/com/example/spring_boot_test/controller/TestController.java
+++ src/main/java/com/example/spring_boot_test/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.spring_boot_test.controller;
 
 import com.example.spring_boot_test.model.Test;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/example/spring_boot_test/filter/CorsFilter.java
+++ src/main/java/com/example/spring_boot_test/filter/CorsFilter.java
@@ -1,25 +1,36 @@
 package com.example.spring_boot_test.filter;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 
 @Configuration
-public class CorsFilter implements Filter {
+public class CorsFilter{
 
-    @Override
-    public void init(FilterConfig filterConfig){
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-    }
-
-    @Override
-    public void destroy() {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            //重写父类提供的跨域请求处理的接口
+            public void addCorsMappings(CorsRegistry registry) {
+                //添加映射路径
+                registry.addMapping("/**")
+                        //放行哪些原始域
+                        .allowedOrigins("*")
+                        //是否发送Cookie信息
+                        .allowCredentials(true)
+                        //放行哪些原始域(请求方式)
+                        .allowedMethods("GET","POST", "PUT", "DELETE")
+                        //放行哪些原始域(头部信息)
+                        .allowedHeaders("*")
+                        //暴露哪些头部信息（因为跨域访问默认不能获取全部头部信息）
+                        .exposedHeaders("Header1", "Header2");
+            }
+        };
     }
 }
 
